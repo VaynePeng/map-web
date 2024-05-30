6c0406ee60da58994b2f2befd78a05d8500e5132
VaynePeng <vaynepeng@gmail.com>
2024-05-30 17:59:01 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import L, { Map } from 'leaflet'
+import { useEffect, useRef } from 'react'
+
 const App = () => {
-  return <div className="text-red-300">Hello world</div>
+  const mapRef = useRef<Map | null>(null)
+
+  useEffect(() => {
+    if (!mapRef.current) {
+      const bounds = new L.LatLngBounds(
+        new L.LatLng(-49.875, 34.25),
+        new L.LatLng(-206, 221)
+      )
+      mapRef.current = L.map('map', {
+        crs: L.CRS.Simple,
+        attributionControl: false,
+        maxBounds: bounds,
+        maxBoundsViscosity: 1.0
+      }).setView([0, 0], 2)
+
+      L.tileLayer('/maps/{z}_{x}_{y}.png', {
+        attribution: '&copy; David',
+        minZoom: 2,
+        maxZoom: 5,
+        noWrap: true,
+        bounds: bounds
+      }).addTo(mapRef.current)
+    }
+  }, [])
+
+  return <div id="map" className="w-screen h-screen"></div>
 }
 
 export default App
